36beb5d9597fcaf7ee0922cc2caa70997d75e60e
multipassport <mashross@gmail.com>
2021-04-17 17:46:54 +0300

    Make function to show current vault visitors

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,19 +1,22 @@
-from datacenter.models import Passcard
-from datacenter.models import Visit
+from datacenter.models import Passcard, Visit
 from django.shortcuts import render
 
 
 def storage_information_view(request):
-    # Программируем здесь
-
-    non_closed_visits = [
-        {
-            "who_entered": "Richard Shaw",
-            "entered_at": "11-04-2018 25:34",
-            "duration": "25:03",
-        }
-    ]
+    people_in_vault = Visit.objects.filter(leaved_at__isnull=True)
+    non_closed_visits = [{
+            'who_entered': person.passcard.owner_name,
+            'entered_at': person.entered_at,
+            'duration': format_duration(get_duration(person))
+            } for person in people_in_vault]
+    
     context = {
         "non_closed_visits": non_closed_visits,  # не закрытые посещения
     }
     return render(request, 'storage_information.html', context)
+
+def get_duration(visit):
+    return 0
+
+def format_duration(duration):
+    return '1ч 1мин'
