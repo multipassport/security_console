6e02356cd57b24a5c46eafd61d941657b16b3d10
multipassport <mashross@gmail.com>
2021-04-18 12:45:30 +0300

    Make visits page to show actual length and suspiciousness of visit

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,21 +1,36 @@
+import datetime
+
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
+from django.utils import timezone
 
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.all()[0]
+    passcard = Passcard.objects.get(passcode=passcode)
     # Программируем здесь
-
-    this_passcard_visits = [
-        {
-            "entered_at": "11-04-2018",
-            "duration": "25:03",
-            "is_strange": False
-        },
-    ]
+    person_visits= Visit.objects.filter(passcard=passcard)
+    this_passcard_visits = [{
+            "entered_at": visit.entered_at,
+            "duration": format_duration(get_duration(visit)),
+            "is_strange": visit.is_visit_long()
+        } for visit in person_visits]
     context = {
         "passcard": passcard,
         "this_passcard_visits": this_passcard_visits
     }
     return render(request, 'passcard_info.html', context)
+
+
+def get_duration(visit):
+    time_in_vault = (visit.leaved_at - visit.entered_at).total_seconds()
+    return time_in_vault
+
+
+def format_duration(duration):
+    hours = int(duration // 3600)
+    minutes = int((duration % 3600) // 60)
+    seconds = int((duration % 3600) % 60)
+    time_in_vault = datetime.time(hour=hours, minute=minutes, second=seconds)
+    return time_in_vault
+
