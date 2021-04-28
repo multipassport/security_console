d70fbf9c36aadb215bb850735245a51da63c1c96
multipassport <mashross@gmail.com>
2021-04-28 19:39:45 +0300

    Remove junk code

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -11,7 +11,7 @@ def passcard_info_view(request, passcode):
     person_visits= Visit.objects.filter(passcard=passcard)
     this_passcard_visits = [{
             "entered_at": visit.entered_at,
-            "duration": format_duration(get_duration(visit)),
+            "duration": visit.format_duration(),
             "is_strange": visit.is_visit_long()
         } for visit in person_visits]
     context = {
@@ -19,19 +19,3 @@ def passcard_info_view(request, passcode):
         "this_passcard_visits": this_passcard_visits
     }
     return render(request, "passcard_info.html", context)
-
-
-def get_duration(visit):
-    if visit.leaved_at:
-        time_in_vault = (visit.leaved_at - visit.entered_at).total_seconds()
-    else:
-        time_in_vault = (timezone.localtime() - visit.entered_at).total_seconds()
-    return time_in_vault
-
-
-def format_duration(duration):
-    hours = int(duration // 3600)
-    minutes = int((duration % 3600) // 60)
-    seconds = int((duration % 3600) % 60)
-    time_in_vault = datetime.time(hour=hours, minute=minutes, second=seconds)
-    return time_in_vault
